059e6326095e850c36426c9ea10099a2a18fa4d1
Yanfei Zhang <lastie@live.cn>
2015-02-23 02:36:59 -0500

    Finished.

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z < 50) {
+			transform.position += new Vector3 (0f, 0f, 3f) * Time.deltaTime;
+		} else if (transform.position.z < 80) {
+			transform.position += new Vector3(0f, 0f, 8f) * Time.deltaTime;
+		} else if (transform.position.z < 180) {
+		transform.position += new Vector3(0f, 0f, 10f) * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Wind.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Wind : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z > 60 && transform.position.z < 130) {
+			GetComponent<ConstantForce>().force = new Vector3(0f, 0f, 10f);
+		}
+		if (transform.position.z > 135) {
+			GetComponent<Rigidbody>().drag = 10000;
+		}
+	}
+}
